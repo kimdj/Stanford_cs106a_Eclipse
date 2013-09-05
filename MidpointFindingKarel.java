66bf9a614e7d2fdf18509e48accbf23adb611efd
David Kim <kim.david.j@gmail.com>
2013-09-05 13:02:00 -0700

    almost finished with problem 4..still needs to work on 8x8 worlds

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -15,30 +15,206 @@ import stanford.karel.*;
 public class MidpointFindingKarel extends SuperKarel {
 
 	public void run() {
-		while (frontIsClear()) {
-			placeBeepers();
-			turnAround();
-			retrieveBeepers();
-			for (int i=0; i<10; i++) {
-				putBeeper();
+		moveToMidpoint();
+		moveToInitial();
+		moveToMidpoint_2();
+	}	
+	
+	/*
+	 * place a beeper if a beeper is not present.
+	 */
+	private void placeBeeper() {
+		if (!beepersPresent()) {
+			putBeeper();
+		}
+	}
+	
+	/*
+	 * move backwards.
+	 */
+	private void moveBackwards() {
+		turnAround();
+		move();
+		turnAround();
+	}
+	
+	/*
+	 * first, move forward; if a beeper is present, move back to initial position
+	 * and turn left.
+	 */
+	private void checkFrontBeeper() {
+		move();
+		if (beepersPresent()) {
+			moveBackwards();
+			turnLeft();
+			move();
+		}
+		
+		if (!frontIsClear()) {
+			placeBeeper();
+			turnLeft();
+			move();
+		}
+	}
+	
+	/*
+	 * 
+	 */
+	private void checkAround() {
+		for (int i=0; i<4; i++) {
+			if (beepersPresent()) {
+				move();
+				moveBackwards();
+				turnLeft();
 			}
 		}
 	}
-
-	private void placeBeepers() {
+	
+	/*
+	 * setting North orientation
+	 */
+	private void setNorth() {
+		
+	}
+	
+	/*
+	 * algorithm for locating the midpoint on 1st street.
+	 */
+	private void moveToMidpoint() {
 		while (frontIsClear()) {
-			putBeeper();
+			placeBeeper();
+			checkFrontBeeper();
+			checkAround();
+			if (beepersPresent()) {
+				move();
+				if (beepersPresent()) {
+					moveBackwards();
+					turnLeft();
+					move();
+					if (beepersPresent()) {
+						moveBackwards();
+						turnLeft();
+						move();
+						if (beepersPresent()) {
+							moveBackwards();
+							turnLeft();
+							move();
+							if (beepersPresent()) {
+								moveBackwards();
+								turnLeft();
+								turnAround();
+								while (frontIsClear()) {
+									move();
+								}
+								turnLeft();
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+/***********************************************************************************************************/
+	
+	/*
+	 * setup initial position.
+	 */
+	private void moveToInitial() {
+		while (!facingWest()) {
+			turnLeft();
+		}
+		
+		while (frontIsClear()) {
 			move();
+		}
+		turnAround();
+	}
+/***********************************************************************************************************/
+	
+	/*
+	 * place a beeper if a beeper is not present.
+	 */
+	private void pickupBeeper() {
+		if (beepersPresent()) {
+			pickBeeper();
+		}
+	}
+	
+	/*
+	 * first, move forward; if a beeper is present, move back to initial position
+	 * and turn left.
+	 */
+	private void checkFrontBeeper_2() {
+		move();
+		if (!beepersPresent()) {
+			moveBackwards();
+			turnLeft();
+			move();
+		}
+		
+		if (!frontIsClear()) {
+			pickupBeeper();
+			turnLeft();
+			move();
+		}
+	}
+	
+	/*
+	 * 
+	 */
+	private void checkAround_2() {
+		for (int i=0; i<4; i++) {
+			if (!beepersPresent()) {
+				move();
+				moveBackwards();
+				turnLeft();
+			}
+		}
+	}
+	
+	/*
+	 * 
+	 */
+	private void moveToMidpoint_2() {
+		while (frontIsClear()) {
+			pickBeeper();
+			checkFrontBeeper_2();
+			checkAround_2();
+			if (!beepersPresent()) {
+				move();
+				if (!beepersPresent()) {
+					moveBackwards();
+					turnLeft();
+					move();
+					if (!beepersPresent()) {
+						moveBackwards();
+						turnLeft();
+						move();
+						if (!beepersPresent()) {
+							moveBackwards();
+							turnLeft();
+							move();
+							if (!beepersPresent()) {
+								moveBackwards();
+								turnLeft();
+								turnAround();
+								while (frontIsClear()) {
+									move();
+								}
+								turnLeft();
+								break;
+							}
+						}
+					}
+				}
+			}
 		}
 		putBeeper();
 	}
 	
-	private void retrieveBeepers() {
-		while (frontIsClear()) {
-			pickBeeper();
-			move();
-		}
-		pickBeeper();
-	}
+	
+	
+	
 }
 
